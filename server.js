6b5a16b18c51be891c727493ed7fdbdad5f96416
zairo984 <developer01zairo@gmail.com>
2025-04-05 11:15:47 +0530

    cors problem

--- server.js
+++ server.js
@@ -9,8 +9,25 @@ const decryptionMiddleware = require("./middlewares/decryptionMiddleware");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+const allowedOrigins = [
+	'https://www.chaloghoomne.com',
+	'http://localhost:5173'
+  ];
+  
+  app.use(cors({
+	origin: function (origin, callback) {
+	  // Allow requests with no origin (like curl or Postman)
+	  if (!origin) return callback(null, true);
+	  if (allowedOrigins.includes(origin)) {
+		return callback(null, true);
+	  } else {
+		return callback(new Error('Not allowed by CORS'));
+	  }
+	},
+	credentials: true // If you're using cookies or Authorization headers
+  }));
+  app.use(express.json({ limit: '50mb' }));
+
 
 app.use(express.urlencoded({ extended: true }));
 
